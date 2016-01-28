acf8d9ba6de192a8f2713beab434885fa363494c
Jacob Hedrick <jacob.hedrick@gmail.com>
2016-01-28 11:03:00 -0600

    Updated path support functions

--- Scripts/Path-Modification-Support.ps1
+++ Scripts/Path-Modification-Support.ps1
@@ -11,27 +11,27 @@ Function global:TEST-LocalAdmin()
      
 Function global:SET-PATH() 
 { 
-[Cmdletbinding(SupportsShouldProcess=$TRUE)] 
-param 
-( 
-[parameter(Mandatory=$True,  
-ValueFromPipeline=$True, 
-Position=0)] 
-[String[]]$NewPath 
-) 
- 
-If ( ! (TEST-LocalAdmin) ) { Write-Host 'Need to RUN AS ADMINISTRATOR first'; Return 1 } 
+    [Cmdletbinding(SupportsShouldProcess=$TRUE)] 
+    param 
+    ( 
+        [parameter(Mandatory=$True,  
+        ValueFromPipeline=$True, 
+        Position=0)] 
+        [String[]]$NewPath 
+    ) 
      
-# Update the Environment Path 
- 
-if ( $PSCmdlet.ShouldProcess($newPath) ) 
-{ 
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath 
- 
-# Show what we just did 
- 
-Return $NewPath 
-} 
+    If ( ! (TEST-LocalAdmin) ) { Write-Host 'Need to RUN AS ADMINISTRATOR first'; Return 1 } 
+         
+    # Update the Environment Path 
+     
+    if ( $PSCmdlet.ShouldProcess($newPath) ) 
+    { 
+        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH' –Value $newPath 
+         
+        # Show what we just did 
+         
+        Return $NewPath 
+    } 
 } 
  
 Function global:ADD-PATH() 
@@ -39,17 +39,17 @@ Function global:ADD-PATH()
 [Cmdletbinding(SupportsShouldProcess=$TRUE)] 
 param 
     ( 
-    [parameter(Mandatory=$True,  
-    ValueFromPipeline=$True, 
-    Position=0)] 
-    [String[]]$AddedFolder 
+        [parameter(Mandatory=$True,  
+        ValueFromPipeline=$True, 
+        Position=0)] 
+        [String[]]$AddedFolder 
     ) 
  
 If ( ! (TEST-LocalAdmin) ) { Write-Host 'Need to RUN AS ADMINISTRATOR first'; Return 1 } 
      
 # Get the Current Search Path from the Environment keys in the Registry 
  
-$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path 
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH').Path 
  
 # See if a new Folder has been supplied 
  
@@ -73,55 +73,55 @@ If (!($AddedFolder[-1] -match '\')) { $Newpath=$Newpath+'\'}
  
 $NewPath=$OldPath+';’+$AddedFolder 
 if ( $PSCmdlet.ShouldProcess($AddedFolder) ) 
-{ 
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath 
- 
-# Show our results back to the world 
- 
-Return $NewPath  
-} 
+    { 
+        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH' –-Value $newPath 
+         
+        # Show our results back to the world 
+         
+        Return $NewPath  
+    } 
 } 
  
 FUNCTION GLOBAL:GET-PATH() 
 { 
-Return (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path 
+    Return (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH').Path 
 } 
  
 Function global:REMOVE-PATH() 
 { 
-[Cmdletbinding(SupportsShouldProcess=$TRUE)] 
-param 
-( 
-[parameter(Mandatory=$True,  
-ValueFromPipeline=$True, 
-Position=0)] 
-[String[]]$RemovedFolder 
-) 
- 
-If ( ! (TEST-LocalAdmin) ) { Write-Host 'Need to RUN AS ADMINISTRATOR first'; Return 1 } 
+    [Cmdletbinding(SupportsShouldProcess=$TRUE)] 
+    param 
+    ( 
+    [parameter(Mandatory=$True,  
+    ValueFromPipeline=$True, 
+    Position=0)] 
+    [String[]]$RemovedFolder 
+    ) 
      
-# Get the Current Search Path from the Environment keys in the Registry 
- 
-$NewPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path 
- 
-# Verify item exists as an EXACT match before removing 
-$Verify=$newpath.split(';') -contains $RemovedFolder 
- 
-# Find the value to remove, replace it with $NULL.  If it’s not found, nothing will change 
- 
-If ($Verify) { $NewPath=$NewPath.replace($RemovedFolder,$NULL) } 
- 
-# Clean up garbage from Path 
- 
-$Newpath=$NewPath.replace(';;',';') 
- 
-# Update the Environment Path 
-if ( $PSCmdlet.ShouldProcess($RemovedFolder) ) 
-{ 
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath 
- 
-# Show what we just did 
- 
-Return $NewPath 
-} 
+    If ( ! (TEST-LocalAdmin) ) { Write-Host 'Need to RUN AS ADMINISTRATOR first'; Return 1 } 
+         
+    # Get the Current Search Path from the Environment keys in the Registry 
+     
+    $NewPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH').Path 
+     
+    # Verify item exists as an EXACT match before removing 
+    $Verify=$newpath.split(';') -contains $RemovedFolder 
+     
+    # Find the value to remove, replace it with $NULL.  If it’s not found, nothing will change 
+     
+    If ($Verify) { $NewPath=$NewPath.replace($RemovedFolder,$NULL) } 
+     
+    # Clean up garbage from Path 
+     
+    $Newpath=$NewPath.replace(';;',';') 
+     
+    # Update the Environment Path 
+    if ( $PSCmdlet.ShouldProcess($RemovedFolder) ) 
+        { 
+            Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name 'PATH' -Value $newPath 
+             
+            # Show what we just did 
+             
+            Return $NewPath 
+        } 
 } 
